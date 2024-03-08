a4ef9a16d5e8c18cd175f8fb7727db2380d37333
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-03-08 13:52:39 +0600

    Add colors, AppColors

--- lib/src/theme/app_colors.dart
+++ lib/src/theme/app_colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 sealed class AppColors {
+  static const black = Colors.black;
   static const white = Colors.white;
+  static const background = Color.fromRGBO(255, 255, 255, 0);
+  static const blue = Color.fromRGBO(133, 195, 222, 1);
 }
