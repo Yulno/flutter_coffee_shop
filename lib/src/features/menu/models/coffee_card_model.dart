7006b94fad7d2dfac8b9a8a0f27b529c2daffde6
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-03-08 13:53:18 +0600

    Add models

---
+++ lib/src/features/menu/models/coffee_card_model.dart
@@ -0,0 +1,9 @@
+class CoffeeCardClass {
+  CoffeeCardClass(this.icon, this.name, this.price);
+  String icon;
+  String name;
+  String price;
+  
+}
+
+var card1 = CoffeeCardClass("assets/removal.png", "Олеато", "139 руб");
---
+++ lib/src/features/menu/models/coffee_title_model.dart
@@ -0,0 +1,9 @@
+class CoffeeTitleClass {
+  CoffeeTitleClass(this.title);
+  String title;
+}
+
+var firstCoffeeTitle = CoffeeTitleClass("Черный кофе");
+var secondCoffeeTitle = CoffeeTitleClass("Кофе с молоком");
+var thirdCoffeeTitle = CoffeeTitleClass("Чай");
+var fourCoffeeTitle = CoffeeTitleClass("Авторские напитки");
