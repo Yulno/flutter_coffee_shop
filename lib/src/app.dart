9d9c5593234472d3da837821ec5d4455a5298f7c
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-03-08 13:55:34 +0600

    Delete baner, add MenuScreen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/src/theme/theme.dart';
+import 'package:flutter_coffee_shop/src/features/menu/view/menu_screen.dart';
+import 'package:flutter_coffee_shop/src/theme/theme.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -9,6 +10,7 @@ class CoffeeShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -18,7 +20,8 @@ class CoffeeShopApp extends StatelessWidget {
       supportedLocales: AppLocalizations.supportedLocales,
       onGenerateTitle: (context) => AppLocalizations.of(context)!.title,
       theme: theme,
-      home: const Center(),
+      home: MenuScreen(),
+      
     );
   }
 }
