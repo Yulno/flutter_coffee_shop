7935e8189b6c5f17fa25e085b36648f644ae47ae
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-03-12 12:39:26 +0600

    Rename bg color

--- lib/src/theme/app_colors.dart
+++ lib/src/theme/app_colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 sealed class AppColors {
   static const black = Colors.black;
   static const white = Colors.white;
-  static const background = Color.fromRGBO(255, 255, 255, 0);
+  static const backgroundWhite = Color.fromRGBO(255, 255, 255, 0);
   static const blue = Color.fromRGBO(133, 195, 222, 1);
 }
