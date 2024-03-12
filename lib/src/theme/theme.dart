269319ae434118da549455b6de62b808d8d8f96b
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-03-12 12:38:47 +0600

    Add CardTheme, fix TextTheme

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -2,46 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_coffee_shop/src/theme/app_colors.dart';
 
 final theme = ThemeData(
-  cardTheme: const CardTheme(),
-  textTheme: const TextTheme(),
-);
-
-TextTheme primaryTextTheme = TextTheme(
-  titleLarge: TextStyle(
-    fontFamily: fontfamily600.text,
+  cardTheme: const CardTheme(
+    color: AppColors.white,
+    margin: EdgeInsets.all(0),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(16)),
+    ),
+  ),
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+    fontFamily: 'Roboto600',
     fontSize: 32,
     color: AppColors.black,
   ),
-  labelLarge: TextStyle(
-    fontFamily: fontfamily500.text,
+  titleMedium: TextStyle(
+    fontFamily: 'Roboto500',
     fontSize: 16,
     color: AppColors.black,
   ),
-  labelMedium: TextStyle(
-    fontFamily: fontfamily400.text,
-    fontSize: 14,
-    color: AppColors.white,
-  ),
-  bodyLarge: TextStyle(
-    fontFamily: fontfamily400.text,
+  bodyMedium: TextStyle(
+    fontFamily: 'Roboto400',
     fontSize: 14,
     color: AppColors.black,
   ),
-  labelSmall: TextStyle(
-    fontFamily: fontfamily400.text,
+  bodySmall: TextStyle(
+    fontFamily: 'Roboto400',
     fontSize: 12,
     color: AppColors.white,
   ),
+  ),
+
+  filledButtonTheme: const FilledButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStatePropertyAll(AppColors.blue),
+    ),
+  ),
 );
 
-class FontFamily {
-  final String? text;
 
-  FontFamily(this.text);
-}
-
-FontFamily fontfamily400 = FontFamily("Roboto400");
-FontFamily fontfamily500 = FontFamily("Roboto500");
-FontFamily fontfamily600 = FontFamily("Roboto600");
 
 
