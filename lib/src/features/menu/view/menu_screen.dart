32cee475bbf3c685d9abe0b08a29affb5bcf0ad4
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-03-12 13:10:02 +0600

    Changed MenuScreen

--- lib/src/features/menu/view/menu_screen.dart
+++ lib/src/features/menu/view/menu_screen.dart
@@ -1,29 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_coffee_shop/src/features/menu/models/coffee_card_model.dart';
 import 'package:flutter_coffee_shop/src/features/menu/models/coffee_title_model.dart';
 import 'package:flutter_coffee_shop/src/features/menu/view/widgets/category.dart';
-import 'package:flutter_coffee_shop/src/features/menu/view/widgets/coffee_title.dart';
-import 'package:flutter_coffee_shop/src/features/menu/view/widgets/coffee_card.dart';
+import 'package:flutter_coffee_shop/src/theme/app_colors.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
+class MenuScreen extends StatefulWidget {
+  final List<CategoryModel> categories;
 
-class MenuScreen extends StatelessWidget {
-  const MenuScreen({super.key});
+  const MenuScreen({super.key, required this.categories});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  late Map<String, GlobalKey> categoryKeys;
+  late String selectedCategoryName;
+  final ItemScrollController _menuController = ItemScrollController();
+  final ItemScrollController _appBarController = ItemScrollController();
+  late ItemPositionsListener itemListener;
+  int current = 0;
+  bool inProgress = false;
+  bool scrolledToBottom = false;
+
+  @override
+  void initState() {
+    super.initState();
+    itemListener = ItemPositionsListener.create();
+
+    categoryKeys = {
+      for (var category in widget.categories) category.categoryName: GlobalKey()
+    };
+
+    itemListener.itemPositions.addListener(() {
+      final fullVisible = itemListener.itemPositions.value.firstWhere((item) {
+        return item.itemLeadingEdge >= 0;
+      }).index;
+
+      if (((fullVisible != current) && inProgress != true) &&
+          scrolledToBottom == false) {
+        setCurrent(fullVisible);
+        appBarScrollToCategory(fullVisible);
+      }
+    });
+  }
+
+  void setCurrent(int newCurrent) {
+    setState(() {
+      current = newCurrent;
+    });
+  }
+
+  void menuScrollToCategory(int ind) async {
+    inProgress = true;
+    _menuController.scrollTo(
+        index: ind, duration: const Duration(milliseconds: 200));
+    await Future.delayed(const Duration(milliseconds: 200));
+    inProgress = false;
+  }
+
+  void appBarScrollToCategory(int ind) async {
+    _appBarController.scrollTo(
+        curve: Curves.easeOut,
+        opacityAnimationWeights: [20, 20, 60],
+        index: ind,
+        duration: const Duration(milliseconds: 300));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:  ListView(
-        children: [
-          Category(),
-          CoffeeTitle(coffeeTitle: firstCoffeeTitle),
-          CoffeeCard(coffeeCard: card1),
-          CoffeeTitle(coffeeTitle: secondCoffeeTitle),
-          CoffeeCard(coffeeCard: card1),
-          CoffeeTitle(coffeeTitle: thirdCoffeeTitle),
-          CoffeeCard(coffeeCard: card1),
-          CoffeeTitle(coffeeTitle: fourCoffeeTitle),
-          CoffeeCard(coffeeCard: card1),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          surfaceTintColor: Colors.transparent,
+          title: PreferredSize(
+            preferredSize: const Size.fromHeight((40)),
+            child: SizedBox(
+              height: 40,
+              child: ScrollablePositionedList.builder(
+                itemScrollController: _appBarController,
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.categories.length,
+                itemBuilder: (context, index) {
+                  final category = widget.categories[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setCurrent(index);
+                        menuScrollToCategory(index);
+                        appBarScrollToCategory(index);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: index == current
+                              ? AppColors.blue
+                              : AppColors.white),
+                      child: Text(
+                        category.categoryName,
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: index == current
+                                ? AppColors.white
+                                : AppColors.black),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: ScrollablePositionedList.builder(
+            itemScrollController: _menuController,
+            itemPositionsListener: itemListener,
+            itemBuilder: (context, index) {
+              final category = widget.categories[index];
+              return Category(
+                key: categoryKeys[category.categoryName],
+                category: category,
+              );
+            },
+            itemCount: widget.categories.length,
+          ),
+        ),
       ),
     );
   }
